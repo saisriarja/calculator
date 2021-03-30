166d94a1641bb7ef51c7fbe23227e26d33cd1163
saisriarja@gmail.com <saisriarja@gmail.com>
2021-03-30 21:33:43 +0530

    9:30 push

--- js/main.js
+++ js/main.js
@@ -1,13 +1,14 @@
-function insrt(num){
+
+function insrt(num) {
     $('.calc-display').val($('.calc-display').val() + num);
 }
-function eql(){
+function eql() {
     $('.calc-display').val(eval($('.calc-display').val()));
 }
 function c() {
-    $('.calc-display').val('');
+    $('.calc-display').val(' ');
 }
 function del() {
     value = $('.calc-display').val();
     $('.calc-display').val(value.substring(0, value.length - 1));
-}
+};
